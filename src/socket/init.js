9b9439b286524d0e87f47abb14d3cdbb501ae6af
Hima <84233494+Himatric@users.noreply.github.com>
2022-09-07 18:09:17 +0200

    Update init.js

--- src/socket/init.js
+++ src/socket/init.js
@@ -6,10 +6,10 @@ const EventEmitter = require("events")
 const { default: axios } = require("axios")
 const emitter = new EventEmitter()
 const { updatePresence } = require('../rpc/rpc')
-const { appendFileSync } = require("fs")
+const { appendFileSync, cp } = require("fs")
 const drawImages = () => {
-    term.drawImage("images/dickord.png", {"shrink": {"height": 110, "width": 200}}, () => {
-        term.drawImage("images/rapecord.png", {shrink: {height: 50, width: 200}})
+    term.drawImage("images/dickord.png", { "shrink": { "height": 110, "width": 200 } }, () => {
+        term.drawImage("images/rapecord.png", { shrink: { height: 50, width: 200 } })
     })
 }
 const InitConsole = async (client) => {
@@ -27,7 +27,7 @@ const InitConsole = async (client) => {
     client.guilds = new NodeCache()
     client.textChannels = new NodeCache()
     client.DMChannels = new NodeCache()
-    if(client.settings.rpc) {
+    if (client.settings.rpc) {
         const rpc = require("discord-rpc")
         client.rpc = new rpc.Client({ "transport": "ipc" })
         client.rpc.on("ready", () => {
@@ -38,11 +38,11 @@ const InitConsole = async (client) => {
     const ws = new WebSocket("wss://gateway.discord.gg/?encoding=json&v=9")
     client.socket = ws
     term.on("key", (key) => {
-        switch(key) {
-            case "CTRL_C": 
+        switch (key) {
+            case "CTRL_C":
                 process.exit(0)
             case "LEFT":
-                if(client.currentState === "inChannel") {
+                if (client.currentState === "inChannel") {
                     client.emitter.emit("return", "")
                     return fase(client, key);
                 }
@@ -71,20 +71,20 @@ const InitConsole = async (client) => {
     }
     ws.onmessage = async (message) => {
         const msg = JSON.parse(message.data.toString())
-        switch(msg.t) {
+        switch (msg.t) {
 
             case "READY":
-                
-                for(const guild of msg.d.guilds) {
+
+                for (const guild of msg.d.guilds) {
                     client.guilds.set(guild.id, guild)
-                    
+
                     guild.channels.forEach(c => {
                         c.messages = new Map()
                         client.textChannels.set(c.id, c)
                     })
-                    
+
                 }
-                
+
                 client.guildPositions = msg.d.user_settings.guild_positions
                 emitter.emit("ready", "")
                 const channels = (await axios({
@@ -94,17 +94,17 @@ const InitConsole = async (client) => {
                         authorization: client.user.token
                     }
                 })).data
-                for(const channel of channels) {
+                for (const channel of channels) {
                     appendFileSync("./test.json", JSON.stringify(channel))
                     channel.messages = new Map()
                     client.DMChannels.set(channel.id, channel)
                 }
                 break;
             case "MESSAGE_CREATE":
-                
-                if(client.settings.renderEmojis) {
+
+                if (client.settings.renderEmojis) {
                     const matches = msg.d.content.match(/(<a?)?:\w+:\d{18}>/)
-                    if(matches) {
+                    if (matches) {
                         const match = matches[0]
                         const id = match.split(":")[2].replace(">", "")
                         const animated = match.split(":")[0] === "<a"
@@ -112,29 +112,31 @@ const InitConsole = async (client) => {
                         msg.d.content = msg.d.content.replace(/(<a?)?:\w+:(\d{18}>)?/, "\n" + await renderImage(url, false, client))
                     }
                 }
-                if(msg.d.mentions[0]) {
+                if (msg.d.mentions[0]) {
                     msg.d.mentions.forEach((m) => {
                         msg.d.content = msg.d.content.replace(`<@${m.id}>`, `@${m.username}#${m.discriminator}`)
                     })
                 }
-                if(client.settings.renderImages) {
-                    if(msg.d.attachments[0]) {
+                if (client.settings.renderImages) {
+                    if (msg.d.attachments[0]) {
                         const url = msg.d.attachments[0].url
 
                         url.endsWith(".png") || url.endsWith(".jpg") || url.endsWith(".jpeg") ? msg.d.content = msg.d.content + "\n" + await renderImage(url, true, client) : null
                     }
-                    const args = msg.d.content.split(" ")
-                    for(const arg of args) {
-                        arg.endsWith(".png") || arg.endsWith(".jpg" || arg.endsWith(".jpeg")) ? msg.d.content = msg.d.content + "\n" + await renderImage(arg, true, client) : null
+                    if (!msg.d.content.includes(" ")) {
+                        const args = msg.d.content.split(" ")
+                        for (const arg of args) {
+                            arg.endsWith(".png") || arg.endsWith(".jpg" || arg.endsWith(".jpeg")) ? msg.d.content = msg.d.content + "\n" + await renderImage(arg, true, client) : null
+                        }
                     }
                 }
                 let channel = client.textChannels.get(msg.d.channel_id)
-                if(!channel) {
+                if (!channel) {
                     channel = client.DMChannels.get(msg.d.channel_id)
- 
+
                     channel.messages.set(msg.d.id, msg.d)
                     client.DMChannels.set(msg.d.channel_id, channel)
-                    if(client.user.id === msg.d.author.id) return
+                    if (client.user.id === msg.d.author.id) return
                     client.notifications.push(msg.d)
                 }
                 channel.messages.set(msg.d.id, msg.d)
@@ -145,11 +147,11 @@ const InitConsole = async (client) => {
     }
 
     setInterval(() => {
-        ws.send(JSON.stringify({op:1, d:null}))
+        ws.send(JSON.stringify({ op: 1, d: null }))
     }, 41250);
 
 
-    
+
 }
 
 
